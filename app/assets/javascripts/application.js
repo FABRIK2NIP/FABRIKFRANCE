2a5ee6d0f56a184b92887655589a8947bd96fddb
FABRIK2NIP <nicolas.pujols2@gmail.com>
2016-01-11 01:49:38 +0100

    essaidot

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,9 +3,9 @@
 
 //= require jquery
 //= require jquery_ujs
-//= require app.js
-//= require TweenMax.min
-//= require MorphSVGPlugin.js
+//= require app-js
+//= require TweenMax-min
+//= require MorphSVGPlugin-js
 //= require sweetalert.min
 //= require TextPlugin.min
 //= require turbolinks
